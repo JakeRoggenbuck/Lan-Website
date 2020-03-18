4e6a38dff2f01426c75f4132cf06d962634ef88c
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2020-03-18 00:21:06 -0700

    Add archives and documents

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <h1>Downloadable archives</h1>
 <?php
 
-$fileList = glob('files/*');
+$fileList = glob('archives/*');
 foreach($fileList as $filename){
     //Use the is_file function to make sure that it is not a directory.
     if(is_file($filename)){
@@ -18,6 +18,20 @@ foreach($fileList as $filename){
     }
 }
 ?>
+<br>
+<h1>Important documents</h1>
+<?php
+
+$fileList = glob('documents/*');
+foreach($fileList as $filename){
+    //Use the is_file function to make sure that it is not a directory.
+    if(is_file($filename)){
+	$size = round(filesize($filename) / 8000, 2);
+	echo "<a href='$filename'><button class='fileXib'><h1 class='title'>$filename</h1><h1 class='size'>$size KB</h1></button></a><br>";
+    }
+}
+?>
+
 
 </body>
 </html>
